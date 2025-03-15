ec44d179c8f29159a1bddf7004c340b208c42d50
saif <saif>
2025-03-15 20:55:39 +0300

    End-Dynamic-Routing

--- app/index.jsx
+++ app/index.jsx
@@ -72,14 +72,11 @@ import { useState, useContext, useEffect } from 'react';
 import { ThemeContext } from '@/context/ThemeContext';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
+import { useRouter } from 'expo-router';
 import { Inter_500Medium, useFonts } from '@expo-google-fonts/inter';
-
 import Animated, { LinearTransition } from 'react-native-reanimated';
-
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
 import { StatusBar } from 'expo-status-bar';
-
 import { Octicons } from '@expo/vector-icons/Octicons';
 
 import { data } from "../data/todos";
@@ -90,6 +87,8 @@ export default function Index() {
   const [todos, setTodos] = useState([]);
   const [text, setText] = useState('');
   const { colorScheme, setColorScheme, Theme } = useContext(ThemeContext);
+  const router = useRouter();
+
   const [loaded, error] = useFonts({
     Inter_500Medium,
   })
@@ -147,27 +146,28 @@ export default function Index() {
     setTodos(todos.filter(todo => todo.id !== id));
   };
 
+  const handlePress = (id) => {
+    router.push(`/todos/${id}`)
+  }
+
+
   const renderItem = ({ item }) => (
-    <View style={styles..todoItem}>
-      <Text style={[styles.todoText, item.completed && styles.completedText]}
-        onPress={() => toggleTodo(item.id)}
+    <View style={styles.todoItem}>
+
+      <Pressable
+        onPress={() => handlePress(item.id)}
+        onLongPress={() => toggleTodo(item.id)}
       >
-        {item.title}
-      </Text>
+        <Text style={[styles.todoText, item.completed && styles.completedText]}
+        >
+          {item.title}
+        </Text>
+      </Pressable>
+
       <Pressable onPress={() => removeTodo(item.id)}>
         <MaterialCommunityIcons name="delete-circle" size={36} color="red"
           selectable={undefined}
         />
-      </Pressable>
-      <Pressable
-        onPress={() => setColorScheme(Color === 'light' ? 'dark' : 'light')}
-        style={{ marginLeft: 10 }}
-      >
-        {colorScheme === 'dark' ?
-          <Octicons name="moon" size={36} color={Theme.Text}
-            selectable={undefined} style={{ width: 36 }} />
-          : <Octicons name="sun" size={36} color={Theme.Text}
-            selectable={undefined} style={{ width: 36 }} />}
       </Pressable>
     </View>
   )
@@ -177,6 +177,7 @@ export default function Index() {
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.input}
+          maxLength={30}
           placeholder="Add a new todo"
           placeholderTextColor="gray"
           value={text}
@@ -185,16 +186,26 @@ export default function Index() {
         <Pressable onPress={addTodo} style={styles.addButton}>
           <Text style={styles.addButtonText}>Add</Text>
         </Pressable>
+
+        <Pressable
+          onPress={() => setColorScheme(Color === 'light' ? 'dark' : 'light')}
+          style={{ marginLeft: 10 }}
+        >
+          <Octicons name={colorScheme === 'dark' ? "moon" : "sun"}
+            size={36} color={Theme.Text}
+            selectable={undefined} style={{ width: 36 }} />
+
+        </Pressable>
       </View>
       <Animated.FlatList
         data={todos}
         renderItem={renderItem}
         keyExtractor={todo => todo.id}
         contentContainerStyle={{ flexGrow: 1 }}
-        itemLayoutAnimation={linearTransition}
+        itemLayoutAnimation={LinearTransition}
         keyboardDismissMode="on-drag"
       />
-      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} /> 
+      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
     </SafeAreaView>
   );
 }
